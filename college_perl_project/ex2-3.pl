9a5dbddcbcbda3a0182eae0bb1ef687fa4e31ca6
KaiYanagisawa <12730ky@gmail.com>
2023-05-06 20:22:49 +0900

    finish_ex2-3

--- college_perl_project/ex2-3.pl
+++ college_perl_project/ex2-3.pl
@@ -10,8 +10,8 @@ sub main
 {
     my $s_code = $ARGV[0];
 
-    # test.list のデータをハッシュに格納する関数
-    my %DataHash = mkDataHash("test.list");
+    # test.list のデータをハッシュに格納する関数 (M1Mac環境上では絶対パスでの指定でないとopen関数が指定ファイルを認識しない)
+    my %DataHash = mkDataHash("/Users/yanagisawakai/college2023_1/data_mining/college_perl_project/test.list");
     my $f_name = $DataHash{$s_code};
     print "$s_code -> $f_name\n";
 }
@@ -20,19 +20,14 @@ sub mkDataHash
 {
     my $input_file = $_[0];
     my %data_hash;
-    open (my $IN, $input_file);
-    my $line = <$IN>;
-    print encode_utf8("$line $input_file");
+    open (my $IN, $input_file) or die "cannot open $input_file \n";
 
     while(my $line = <$IN>)
     {
-        print encode_utf8("111");
+        chomp($line);
         my $str = decode_utf8($line);
-        chomp($str);
-        print encode_utf8("$str\n");
         my @cord_file_name = split(/,/, $str);
-        $data_hash{$cord_file_name[1]} = $cord_file_name[0];
-        print encode_utf8("$cord_file_name[0], $cord_file_name[1]");
+        $data_hash{$cord_file_name[0]} = $cord_file_name[1];
     }
     close ($IN);
 
