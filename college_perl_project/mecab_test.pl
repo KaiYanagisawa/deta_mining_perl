0eb0f938b6e2516c51bbca1affaab8073fcc6265
KaiYanagisawa <12730ky@gmail.com>
2023-08-03 11:18:11 +0900

    mecab_test

--- college_perl_project/mecab_test.pl
+++ college_perl_project/mecab_test.pl
@@ -5,32 +5,33 @@ use Encode;
 use utf8;
 
 # 形態素解析器 MeCab
-use MeCab;
+# use MeCab;
 
 # MeCabを宣言（一度、宣言すればよい）
-my $model = new MeCab::Model();
-my $c = $model->createTagger();
+# my $model = new MeCab::Model();
+# my $c = $model->createTagger();
 
 main();
 
 sub main()
 {
-    my $string_utf8 = $ARGV[0];
+    # my $string_utf8 = $ARGV[0];
 
     # MeCabを実行（出力はUTF-8なので、Perlの内部コードに変換する必要有り）
-    my $mecab_results = decode_utf8($c->parse($string_utf8));
+    # my $mecab_results = decode_utf8($c->parse($string_utf8));
 
     # print encode_utf8("$mecab_results");
-    my @POS = split(/\n/,$mecab_results);
+    # my @POS = split(/\n/,$mecab_results);
+    my @POS = ("本日 名詞,副詞可能,*,*,*,*,本日,ホンジツ,ホンジツ", "は 助詞,係助詞,*,*,*,*,は,ハ,ワ");
 
     foreach my $result (@POS)
     {
-        my @data = ;
-        my @pos = ;
+        my @data = split(/\t/, $result);
+        my @pos = split(/,/, $data[1]);
         
-        if ()
+        if ($pos[0] eq "名詞")
         {
-            print encode_utf8("$result \n");
+            print encode_utf8("$data[0]\n");
         }
     }
 }
